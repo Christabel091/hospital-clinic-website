9a61a7fb7066612307b6ec59ff7268256a87622d
Christabel <obinwosugosiorah@gmail.com>
2024-05-28 20:15:47 -0400

    scripting files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Example</title>
+</head>
+<body>
+    <h1>Welcome to my PHP page</h1>
+    <?php
+    echo "code goes here.";
+    $x = 7;
+    
+    ?>
+</body>
+</html>
